747a7d29a2b923a180ff5eac88fd6ed2fc7414c4
Sai Varun Somi Setty <saivarun7032@gmail.com>
2025-09-12 11:50:58 +0530

    Update page.js

--- src/app/(main)/requests/page.js
+++ src/app/(main)/requests/page.js
@@ -472,7 +472,7 @@ export default function RequestsPage() {
                                     onChange={handleInputChange}
                                     className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                                     placeholder="Enter Project code"
-                                    disabled
+                                
                                 />
                             </div>
 
